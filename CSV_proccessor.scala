c439bab12d5de0b705cf77494c1e42dedea44fae
priyankakarale7 <110196022+priyankakarale7@users.noreply.github.com>
2023-05-17 10:27:24 +0530

    Update CSV_proccessor.scala

--- CSV_proccessor.scala
+++ CSV_proccessor.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.rdd.RDD
+
+
 
 class CSV_proccessor {
   val spark = SparkSession.builder()
@@ -13,12 +16,61 @@ class CSV_proccessor {
   df.createOrReplaceTempView("employee_data");
 
   val unique_employee = df.groupBy("empId")
-  
+
 
   unique_employee.max("empSal")
 
   val total_expenditure: DataFrame = spark.sql("select sum(empSal) from employee_data")
   total_expenditure.show()
 
+  spark.sparkContext.setLogLevel("ERROR")
+
+  println("spark read csv files from a directory into RDD")
+  val rddFromFile = spark.sparkContext.textFile("C:/tmp/files/text01.csv")
+  println(rddFromFile.getClass)
+
+  val rdd = rddFromFile.map(f=>{
+    f.split(",")
+  })
+
+  println("Iterate RDD")
+  rdd.foreach(f=>{
+    println("Col1:"+f(0)+",Col2:"+f(1))
+  })
+  println(rdd)
+
+  println("Get data Using collect")
+  rdd.collect().foreach(f=>{
+    println("Col1:"+f(0)+",Col2:"+f(1))
+  })
+
+
+}
+
+object ReadMultipleCSVFiles extends App {
+
+  val spark:SparkSession = SparkSession.builder()
+    .master("local[1]")
+    .appName("ReadMultipleCSV_files")
+    .getOrCreate()
+
+
+  println("read all csv files from a directory to single RDD")
+  val rdd2 = spark.sparkContext.textFile("C:/tmp/files/*")
+  rdd2.foreach(f=>{
+    println(f)
+  })
+
+  println("read csv files base on wildcard character")
+  val rdd3 = spark.sparkContext.textFile("C:/tmp/files/text*.csv")
+  rdd3.foreach(f=>{
+    println(f)
+  })
+
+  println("read multiple csv files into a RDD")
+  val rdd4 = spark.sparkContext.textFile("C:/tmp/files/text01.csv,C:/tmp/files/text02.csv")
+  rdd4.foreach(f=>{
+    println(f)
+  })
 
 }
