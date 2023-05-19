c64987d87068fe8316abaf1efcd368cb6b4e271f
priyankakarale7 <110196022+priyankakarale7@users.noreply.github.com>
2023-05-19 17:54:46 +0530

    Update Union.scala

--- Union.scala
+++ Union.scala
@@ -45,3 +45,75 @@ class Union {
 object Union extends App{
   val un = new Union
 }
+
+Output :
+
+root
+ |-- employee_name: string (nullable = true)
+ |-- department: string (nullable = true)
+ |-- state: string (nullable = true)
+ |-- salary: integer (nullable = false)
+ |-- age: integer (nullable = false)
+ |-- bonus: integer (nullable = false)
+
++-------------+----------+-----+------+---+-----+
+|employee_name|department|state|salary|age|bonus|
++-------------+----------+-----+------+---+-----+
+|        James|     Sales|   NY| 90000| 34|10000|
+|      Michael|     Sales|   NY| 86000| 56|20000|
+|       Robert|     Sales|   CA| 81000| 30|23000|
+|        Maria|   Finance|   CA| 90000| 24|23000|
++-------------+----------+-----+------+---+-----+
+
++-------------+----------+-----+------+---+-----+
+|employee_name|department|state|salary|age|bonus|
++-------------+----------+-----+------+---+-----+
+|James        |Sales     |NY   |90000 |34 |10000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|Jen          |Finance   |NY   |79000 |53 |15000|
+|Jeff         |Marketing |CA   |80000 |25 |18000|
+|Kumar        |Marketing |NY   |91000 |50 |21000|
++-------------+----------+-----+------+---+-----+
+
+Using Union
++-------------+----------+-----+------+---+-----+
+|employee_name|department|state|salary|age|bonus|
++-------------+----------+-----+------+---+-----+
+|James        |Sales     |NY   |90000 |34 |10000|
+|Michael      |Sales     |NY   |86000 |56 |20000|
+|Robert       |Sales     |CA   |81000 |30 |23000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|James        |Sales     |NY   |90000 |34 |10000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|Jen          |Finance   |NY   |79000 |53 |15000|
+|Jeff         |Marketing |CA   |80000 |25 |18000|
+|Kumar        |Marketing |NY   |91000 |50 |21000|
++-------------+----------+-----+------+---+-----+
+
+After removing duplicates
++-------------+----------+-----+------+---+-----+
+|employee_name|department|state|salary|age|bonus|
++-------------+----------+-----+------+---+-----+
+|James        |Sales     |NY   |90000 |34 |10000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|Michael      |Sales     |NY   |86000 |56 |20000|
+|Robert       |Sales     |CA   |81000 |30 |23000|
+|Jeff         |Marketing |CA   |80000 |25 |18000|
+|Jen          |Finance   |NY   |79000 |53 |15000|
+|Kumar        |Marketing |NY   |91000 |50 |21000|
++-------------+----------+-----+------+---+-----+
+
+Using UnionAll
++-------------+----------+-----+------+---+-----+
+|employee_name|department|state|salary|age|bonus|
++-------------+----------+-----+------+---+-----+
+|James        |Sales     |NY   |90000 |34 |10000|
+|Michael      |Sales     |NY   |86000 |56 |20000|
+|Robert       |Sales     |CA   |81000 |30 |23000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|James        |Sales     |NY   |90000 |34 |10000|
+|Maria        |Finance   |CA   |90000 |24 |23000|
+|Jen          |Finance   |NY   |79000 |53 |15000|
+|Jeff         |Marketing |CA   |80000 |25 |18000|
+|Kumar        |Marketing |NY   |91000 |50 |21000|
++-------------+----------+-----+------+---+-----+
